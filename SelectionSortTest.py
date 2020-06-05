e8075cffbbca7d77776de7cd82d20cecd6a38329
Blim-creates <brandonbal76@gmail.com>
2020-06-05 09:21:40 -0700

    Commented Selection Sort

--- SelectionSortTest.py
+++ SelectionSortTest.py
@@ -8,21 +8,20 @@ def selectionsort(array)
     for i in range(n)
 
     #initially assume the first element of the unsorted part as the minimum
-#creating the minimum value as 1
-        minimum = 1
-
+#creating the minimum value as i
+        minimum = i
+        #for loop
         for j in range(i+1, n):
 
             if (array[j] < array[minimum]): #comparison operator that is nested
-
+            #Update position of minimum element if a smaller element is found.
             minimum = j
 
         #swap the minimum element with the first element of the unsorted part
-
         temp = array[i]
         array[i] = array[minimum]
         array[minimum] = temp
 
     return array
-
+#printing the selection sort array
 print(selectionSort(array))
